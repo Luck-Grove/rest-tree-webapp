a675c1e9b2e6adfcdea36d4c5eaa384796d5187b
Christian Kubler <ckubler@luckgrove.com>
2024-10-14 12:03:02 -0400

    fix clear bounding box

--- src/commands/bbclear.js
+++ src/commands/bbclear.js
@@ -1,10 +1,12 @@
 import { writeToConsole } from '../utils/api.js';
+import { lastBoundingBoxCoordinates } from './bbox.js';
 
 const bbclearCommand = {
   execute: (map, args, commandList, addConsoleMessage) => {
     if (map && map.boundingBox) {
       map.removeLayer(map.boundingBox);
       map.boundingBox = null;
+      lastBoundingBoxCoordinates = null;
       writeToConsole("Bounding box cleared.", addConsoleMessage);
     } else {
       writeToConsole("No bounding box to clear.", addConsoleMessage);
